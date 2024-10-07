bc589e3502b1184a60e908799bbd3fb2a6cba858
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-10-07 22:45:01 +0200

    Add MoreGatherers.distinctBy() (#9)

--- src/main/java/com/pivovarit/gatherers/MoreGatherers.java
+++ src/main/java/com/pivovarit/gatherers/MoreGatherers.java
@@ -1,7 +1,10 @@
 package com.pivovarit.gatherers;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Function;
 import java.util.stream.Gatherer;
 
 import static java.util.stream.Gatherer.Integrator.ofGreedy;
@@ -10,6 +13,19 @@ import static java.util.stream.Gatherer.ofSequential;
 public final class MoreGatherers {
 
     private MoreGatherers() {
+    }
+
+    public static <T, U> Gatherer<T, ?, T> distinctBy(Function<? super T, ? extends U> keyExtractor) {
+        Objects.requireNonNull(keyExtractor);
+        return Gatherer.ofSequential(
+          () -> new HashSet<U>(),
+          (state, element, downstream) -> {
+              if (state.add(keyExtractor.apply(element))) {
+                  downstream.push(element);
+              }
+              return true;
+          }
+        );
     }
 
     public static <T> Gatherer<T, ?, Map.Entry<Long, T>> zipWithIndex() {
