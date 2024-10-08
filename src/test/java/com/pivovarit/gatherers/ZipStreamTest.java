c49ee0f8b01ea1bedf1ec4081e89ca68c6c3d5f9
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-10-08 08:57:13 +0200

    Update README (#15)

--- src/test/java/com/pivovarit/gatherers/ZipStreamTest.java
+++ src/test/java/com/pivovarit/gatherers/ZipStreamTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.stream.Stream;
 
+import static com.pivovarit.gatherers.MoreGatherers.*;
 import static java.util.Map.entry;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,17 +12,17 @@ class ZipStreamTest {
 
     @Test
     void shouldZipEmpty() {
-        assertThat(Stream.empty().gather(MoreGatherers.zip(Stream.of(1, 2, 3)))).isEmpty();
+        assertThat(Stream.empty().gather(zip(Stream.of(1, 2, 3)))).isEmpty();
     }
 
     @Test
     void shouldZipWithEmpty() {
-        assertThat(Stream.of(1, 2, 3).gather(MoreGatherers.zip(Stream.of()))).isEmpty();
+        assertThat(Stream.of(1, 2, 3).gather(zip(Stream.of()))).isEmpty();
     }
 
     @Test
     void shouldZip() {
-        assertThat(Stream.of(1, 2, 3).gather(MoreGatherers.zip(Stream.of("a", "b", "c", "d"))))
+        assertThat(Stream.of(1, 2, 3).gather(zip(Stream.of("a", "b", "c", "d"))))
           .containsExactly(
             entry(1, "a"),
             entry(2, "b"),
