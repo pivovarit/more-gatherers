fe76c39eb51a24a6010e54feaf344bc221b7cfeb
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-10-22 09:28:39 +0200

    update

--- src/main/java/com/pivovarit/jdd/JDD.java
+++ src/main/java/com/pivovarit/jdd/JDD.java
@@ -4,13 +4,10 @@ package com.pivovarit.jdd;
  * JEP 461: Breathing New Life into Java Stream API
  * {@code s/461/485/g
  * <p>
+ *
  * @author pivovarit
  */
 class JDD {
-    record E1() {
-        public static void main(String[] args) {
-
-        }
+    public static void main(String[] args) {
     }
-
 }
