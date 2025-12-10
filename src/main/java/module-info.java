cf441d15042f9b3238356877cd9967afac0ce7bd
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2025-12-10 09:43:15 +0400

    JSpecify integration (#154)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,6 @@
 /// Missing Stream API functionality you always longed for - provided via `Gatherers`
 module com.pivovarit.gatherers {
     exports com.pivovarit.gatherers;
+
+    requires static org.jspecify;
 }
